c0a8682af1dda810dea58de60e9ba387aaa725d4
Gleb Liutsko <gleb@liutsko.ru>
2021-05-11 19:49:13 +0400

    2 pr

---
+++ Dvym2/Dvym2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Dvym2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Получаем размер матрицы
+            Console.Write("Размер матрицы: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Инициализируем 2мерный массив
+            int[,] matrix = new int[n, n];
+            // Заполняем его значениями, введенные с консоли
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(); // Просто вывод пустой строки, что бы отделить вывод матрицы
+            // Вывод на консоль
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            int s = 0;  // Перменная, хранящая сумму
+            // По задачи нам надо найти сумму нижнией части матрицы, разделенной главной диогональю.
+            // На каждой следующий строки матрицы нам нужно сложить на 1 элемент больше. Т.е. на 1-ой строке мы складываем 1 элемент, на 2-ой строке 2 элемента и т.д.
+            for (int i = 0; i < n; i++)
+            {
+                // i + 1, т.к. ноумерация начинается с 0. Либо можно было написать j <= i
+                for (int j = 0; j < i + 1; j++)
+                    s += matrix[i, j];
+            }
+            Console.WriteLine($"Сумма выделеных элементов: {s}");
+        }
+    }
+}
--- Dvym2/Dvym2/Properties/AssemblyInfo.cs
+++ Dvym2/Dvym2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Dvym1")]
+[assembly: AssemblyTitle("Dvym2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dvym1")]
+[assembly: AssemblyProduct("Dvym2")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("95964b36-841f-4745-9260-f457a6acc9e3")]
+[assembly: Guid("91b04bfc-fbe7-447e-a718-2b0b81474f70")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
